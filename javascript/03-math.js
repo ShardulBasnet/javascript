348c86f1a7ff5d2b3b7698a6f3f52535f77a9a59
Shardul Basnet <120680007+ShardulBasnet@users.noreply.github.com>
2024-02-05 14:23:36 +0000

    maths

--- javascript/03-math.js
+++ javascript/03-math.js
@@ -2,3 +2,8 @@ console.log( Math.round(Math.random()*10))
 console.log(Math.round(4.6)) /*output=5 */
 console.log(Math.ceil(4.2)) /* output=5 */
  console.log(Math.floor(4.8)) /*output =4 */
+//  if u have to get the value between two numbers
+const max = 20;
+const min = 10;
+let value =(Math.floor(Math.random()*(max -min+1))+min)
+console.log(value)
