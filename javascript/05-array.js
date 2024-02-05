95cf45dd41cb5235429f4cb230804c93831ab89c
Shardul Basnet <120680007+ShardulBasnet@users.noreply.github.com>
2024-02-05 14:22:12 +0000

    array basics

---
+++ javascript/05-array.js
@@ -0,0 +1,11 @@
+let array_first=[1,2,3,4]
+let array_second=[5,6,7,8]
+let combined_array=[...array_first,...array_second] /* spread operator */
+// console.log(combined_array);/*output =[1,2,3,4,5,6,7,8]
+
+/* Array methods */
+let array_third=["shardul","basnet",1,2]
+let pushed_array=array_third.push(6)
+console.log(pushed_array) /* it gives the total length of an array i.e 5 */
+console.log(array_third)/*adds the pushed up array value at the last*/
+
