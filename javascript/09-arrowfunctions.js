a2a41672ad6fa3211ce7462fc6e30433e91c24ad
Shardul Basnet <120680007+ShardulBasnet@users.noreply.github.com>
2024-02-12 07:53:14 +0000

    arrow functions

---
+++ javascript/09-arrowfunctions.js
@@ -0,0 +1,9 @@
+// const add =(num1,num2)=>{
+    // return num1 + num2
+// }
+// let sum=add(3,4)
+// console.log(sum)
+// same as this one
+const add =(num1,num2)=> (num1+num2)
+let sum=add(3,4)
+console.log(sum)
