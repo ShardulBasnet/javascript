6219e44bec86d6af94455601df1561877b48707b
Shardul Basnet <120680007+ShardulBasnet@users.noreply.github.com>
2024-02-15 02:37:28 +0000

    loop with break and continue

---
+++ javascript/12-loops.js
@@ -0,0 +1,18 @@
+for (let i = 0;i <10;i++) {
+if (i===3){
+    continue
+
+}
+    console.log(i)
+    
+}
+/* this will print all except 3 */
+for (let i = 0;i <10;i++) {
+    if (i===3){
+        break
+    
+    }
+        console.log(i)
+        
+    }
+    /* this will print upto 2 i.e 0,1,2 */
