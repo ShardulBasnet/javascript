dbf26e9bd71a97b0dc57248cf68d21d5883a7063
Shardul Basnet <120680007+ShardulBasnet@users.noreply.github.com>
2024-02-23 15:41:59 +0000

    setInterval

---
+++ javascript/async project/async.js
@@ -0,0 +1,23 @@
+const randomColor=function(){
+    let hex= "0123456789ABCDEF";
+    let color = "#"
+    for(let i=0;i<6;i++){
+        color +=hex[Math.floor(Math.random()*16)]
+    }
+    return color;
+
+}
+let changecolor=function(){
+    document.body.style.backgroundColor=randomColor()
+}
+let intervalId;
+let startchangingcolor=function(){
+    intervalId=setInterval(changecolor,1000)
+
+}
+let stopcolor=function(){
+    clearInterval(intervalId)
+    
+}
+document.querySelector("#changecolor").addEventListener('click',startchangingcolor)
+document.querySelector("#stopcolor").addEventListener('click',stopcolor)
